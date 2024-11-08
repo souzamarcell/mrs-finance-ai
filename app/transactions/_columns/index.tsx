038c73ff46013ab7d7c1afe2781f8cfef191449d
Marcell Souza <marcell.ramos@gmail.com>
2024-11-08 10:00:29 -0400

    feat: party 1.0.2 - finalizando aula 3

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,31 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { TrashIcon } from "lucide-react";
+import EditTransactionButton from "../_components/edit-transaction-button";
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  OTHER: "Outros",
+  PIX: "Pix",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,46 +44,49 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
-    //   cell: ({ row: { original: transaction } }) =>
-    //     TRANSACTION_CATEGORY_LABELS[transaction.category],
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
-    //   cell: ({ row: { original: transaction } }) =>
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
+    // cell: ({ row: { original: transaction } }) =>
+    // transactionCategoryMap[transaction.category],
     //     TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
-    // cell: ({ row: { original: transaction } }) =>
-    //   new Date(transaction.date).toLocaleDateString("pt-BR", {
-    //     day: "2-digit",
-    //     month: "long",
-    //     year: "numeric",
-    //   }),
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
-    // cell: ({ row: { original: transaction } }) =>
-    //   new Intl.NumberFormat("pt-BR", {
-    //     style: "currency",
-    //     currency: "BRL",
-    //   }).format(Number(transaction.amount)),
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
     header: "Ações",
-    // cell: ({ row: { original: transaction } }) => {
-    //   return (
-    //     <div className="space-x-1">
-    //       <EditTransactionButton transaction={transaction} />
-    //       <Button variant="ghost" size="icon" className="text-muted-foreground">
-    //         <TrashIcon />
-    //       </Button>
-    //     </div>
-    //   );
-    // },
+    cell: ({ row: { original: transaction } }) => {
+      return (
+        <div className="space-x-1">
+          <EditTransactionButton transaction={transaction} />
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
@@ -13,11 +12,8 @@ const TransactionsPage = async () => {
       ))} */}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button>
-          Adicionar transação
-          <ArrowDownUpIcon />
-        </Button>
-        {/* <AddTransactionButton /> */}
+
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
     </div>
